d73714ce3c50e6f6438b7d57a4900c48ad9486a7
Luís Eduardo <eduardovansilva@gmail.com>
2020-09-04 09:32:58 -0300

    feat: Integrate MYSQL db container into spring application and create Market Model

---
+++ src/main/java/com/inatel/stockquotemanager/models/Market.java
@@ -0,0 +1,39 @@
+package com.inatel.stockquotemanager.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="MARKET")
+public class Market {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	
+	private String name;
+	private String description;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}	
+}
